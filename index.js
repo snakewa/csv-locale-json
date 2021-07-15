3e29f70c758c0287e1b6a756de865d70814b2aa7
Tim Ma <tim@wizmacau.com>
2021-07-15 13:58:15 +0800

    Init commit

---
+++ index.js
@@ -0,0 +1,52 @@
+#! /usr/bin/env node
+const fs = require("fs").promises;
+const parse = require("csv-parse/lib/sync");
+const fetch = require("node-fetch");
+
+(async function () {
+  const { program } = require("commander");
+  program.version("0.0.1");
+  program
+    .option("-d, --debug", "output extra debugging")
+    .requiredOption("-u, --csv-url <url>", "csv url")
+    .requiredOption("-o, --output <output>", "output folder");
+  program.parse(process.argv);
+  const options = program.opts();
+  
+
+  // Read the content
+  console.log("fetching...")
+  const response = await fetch(options.csvUrl);
+  const content = await response.text();
+
+  
+  // Parse the CSV content
+  const records = parse(content);
+  // Print records to the console
+  const zipObject = (props, values) => {
+    return props.reduce((prev, prop, i) => {
+      return Object.assign(prev, { [prop]: values[i] });
+    }, {});
+  };
+  const headers = records.shift();
+  const langs = headers.slice(1);
+  let db = {};
+  langs.forEach((lang) => {
+    db[lang] = {};
+  });
+  records.forEach((record) => {
+    let data = zipObject(headers, record);
+    const key = data["key"];
+    langs.forEach((lang) => {
+      db[lang][key] = data[lang];
+    });
+  });
+  langs.forEach((lang) => {
+    const target = `${options.output}/${lang}.json`;
+    console.log(`saving ${target}...`);
+    fs.writeFile(
+      target,
+      JSON.stringify(db[lang], "", 2)
+    );
+  });
+})();
